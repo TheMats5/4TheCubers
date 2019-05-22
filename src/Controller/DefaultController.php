fa6cfee8b2ec024df14235b83a1470dfc178c665
TheMats5 <mats.vingerhoets5@gmail.com>
2019-05-22 17:58:13 +0200

    ajax time post

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -10,10 +11,14 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request)
     {
+        if(null!==$request->get('time')){
+            dd($request->get('time'));
+        }
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }
+
 }
