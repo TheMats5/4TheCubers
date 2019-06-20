827a4225cb3c54ec3c76bc667610796d59c2e37c
TheMats5 <mats.vingerhoets5@gmail.com>
2019-06-20 06:19:49 +0200

    edit profile on stats page

--- src/Controller/StatsController.php
+++ src/Controller/StatsController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UpdateProfileForm;
 use App\Repository\FriendsRepository;
 use App\Repository\SolveRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -19,15 +20,35 @@ class StatsController extends AbstractController
      * @Route("/stats", name="stats")
      * @Template
      */
-    public function stats(FriendsRepository $friendsRepository, SolveRepository $solveRepository)
+    public function stats(FriendsRepository $friendsRepository, SolveRepository $solveRepository, Request $request)
     {
         /** @var User $user */
         $user = $this->getUser();
+        $form = $this->createForm(UpdateProfileForm::class, $user);
 
         $friendRequests = $friendsRepository->getRequestedFriendships($user);
         $amountOfRequest = count($friendRequests);
         $allStats = $solveRepository->getAllSolvesByUser($user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $file = $request->files->get('update_profile_form')['profilePicture'];
+            if(null !== $file){
+                $uploads_directory = $this->getParameter('uploads_directory');
+                $filename = md5(uniqid()) . '.' . $file->guessExtension();
+                $file->move(
+                    $uploads_directory,
+                    $filename
+                );
+                $user->setProfilePicture("uploads/".$filename);
+            }
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('friends');
+        }
         return $this->render('default/stats.html.twig',[
+            'profileForm' => $form->createView(),
             'allStats' => $allStats,
             'user' => $user,
             'amountOfRequest' => $amountOfRequest,
